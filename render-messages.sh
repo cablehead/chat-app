b6052abf32d94172c5d1c39e51a0a51ce69e7cb1
Andy Gayton <andy@thecablelounge.com>
2023-12-20 17:34:39 -0500

    wip

--- render-messages.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-BASE="$(dirname "$0")"
-cd "$BASE"
-
-STORE="$1"
-tail -n20 "$STORE"/messages.json | jq -s 'reverse | {messages: .}' | tera -t ./html/messages.html --stdin
